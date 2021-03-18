13082d9e3986bb4d3a3994302b225888279ae124
Denis Belik <denis.belik2014@gmail.com>
2021-03-18 16:47:04 +0200

    make dropdown animations more appealing

--- src/animations/dropdown.js
+++ src/animations/dropdown.js
@@ -8,67 +8,71 @@ function getElements(element) {
     }
 }
 
-function showDropdown(element) {
-    console.log('here')
+function showDropdown(element, timelineStop, timelineShow) {
+    console.log('show')
+    // Prepare to start animation
+    timelineStop.pause();
+    timelineStop.clear();
+    timelineShow.play();
+
     const elements = getElements(element);
 
-    gsap.to(elements.arrow, {
+    timelineShow.to(elements.arrow, {
         rotate: 180,
         duration: .2
     });
     
-    gsap.to(elements.itemsContainer, {
+    timelineShow.to(elements.itemsContainer, {
         display: 'flex',
         duration: 0,
     });
-    gsap.to(elements.itemsContainer, {
+    timelineShow.to(elements.itemsContainer, {
         height: "auto",
         duration: .2
     });
-    gsap.to(elements.items, {
-        paddingTop: "30px",
-        paddingBottom: "30px",
-        duration: .2
-    });
-    gsap.to(elements.items, {
+    timelineShow.to(elements.items, {
         opacity: 1,
         duration: .2,
-        delay: .2
     });
 }
 
-function closeDropdown(element) {
+function closeDropdown(element, timelineStop, timelineShow) {
+    console.log('stop')
+    timelineShow.pause();
+    timelineShow.clear();
+    timelineStop.play();
+
     const elements = getElements(element);
     
-    gsap.to(elements.items, {
+    timelineStop.to(elements.items, {
         opacity: 0,
         duration: .2,
     });
-    gsap.to(elements.itemsContainer, {
+    timelineStop.to(elements.itemsContainer, {
         height: "0",
         duration: .2,
         paddingTop: "0",
         paddingBottom: "0",
-        delay: .2
     });
-    gsap.to(elements.itemsContainer, {
+    timelineStop.to(elements.itemsContainer, {
         display: 'none',
         duration: 0,
-        delay: .4
     });
-    gsap.to(elements.arrow, {
+    timelineStop.to(elements.arrow, {
         rotate: 0,
         duration: .2,
-        delay: .4
     });
 }
 
 window.addEventListener('load', () => {
     const elements = document.getElementsByClassName('dropdown-navbar');
     Array.from(elements).forEach((dropdown) => {
-        dropdown.addEventListener('mouseenter', () => showDropdown(dropdown));
-        dropdown.addEventListener('mouseleave', () => closeDropdown(dropdown));
-        dropdown.addEventListener('focusin', () => showDropdown(dropdown));
-        dropdown.addEventListener('focusout', () => closeDropdown(dropdown));
+        const timelineStop = gsap.timeline();
+        const timelineShow = gsap.timeline();
+
+        dropdown.addEventListener('mouseenter', () => showDropdown(dropdown, timelineStop, timelineShow));
+        dropdown.addEventListener('mouseleave', () => closeDropdown(dropdown, timelineStop, timelineShow));
+        dropdown.addEventListener('focusin', () => showDropdown(dropdown, timelineStop, timelineShow));
+        dropdown.addEventListener('focusout', () => closeDropdown(dropdown, timelineStop, timelineShow));
     })
 })
